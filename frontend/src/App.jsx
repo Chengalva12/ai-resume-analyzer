3a7a79bd63b0bd1fedf53761917bb56b19db5e49
Chengalva12 <chengalvasharanya@gmail.com>
2026-05-25 22:30:35 -0500

    Connect frontend with deployed backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
     formData.append("job_description", jobDescription);
 
     const response = await axios.post(
-      "http://127.0.0.1:8000/analyze",
+     "https://ai-resume-analyzer-backend-7o6y.onrender.com/analyze"
       formData
     );
 
